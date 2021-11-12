bd51946f041237cafb1c880014819b7e3c79a51b
korolvitalii <korolvitalik@gmail.com>
2021-11-12 07:52:33 +0100

    --

--- src/styled/Home.styled.tsx
+++ src/styled/Home.styled.tsx
@@ -1,5 +1,5 @@
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { Paper, TextField, Typography } from '@mui/material';
+import { Button, Paper, TextareaAutosize, TextField, Typography } from '@mui/material';
 import { styled } from '@mui/styles';
 
 export const SearchTextField = styled(TextField)({
@@ -8,8 +8,11 @@ export const SearchTextField = styled(TextField)({
   },
   '& .MuiOutlinedInput-root': {
     '& fieldset': {
-      borderRadius: '30px',
-      backgroundColor: '#E6ECF0',
+      borderRadius: '25px',
+      // backgroundColor: '#E6ECF0',
+    },
+    '& svg path': {
+      fill: 'red',
     },
     '& .MuiInput-underline:after': {
       borderBottomColor: '#E6ECF0',
@@ -38,10 +41,13 @@ export const SideMenuItems = styled('ul')({
 export const SideMenuItem = styled('li')({
   display: 'inline-flex',
   alignItems: 'center',
-  width: '180px',
+  width: '100%',
   borderRadius: 30,
   padding: '0 10px',
+  marginBottom: 15,
   height: 40,
+  cursor: 'pointer',
+  transition: 'background-color 0.15s ease-in-out',
   '& p': {
     paddingLeft: 15,
   },
@@ -83,4 +89,42 @@ export const TweetIcons = styled('div')({
   justifyContent: 'space-between',
   marginLeft: 25,
   marginRight: 25,
+});
+
+export const SideMenuButtonTweet = styled(Button)({
+  // width: '180px',
+});
+
+export const AddForm = styled('div')({
+  padding: 10,
+});
+
+export const AddFormBody = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  width: '100%',
+});
+
+export const AddFormTextarea = styled(TextareaAutosize)({
+  width: '100%',
+  border: 0,
+  marginLeft: 5,
+  fontSize: 20,
+  outlne: 'none',
+  fontFamily: 'inherit',
+  resize: 'none',
+});
+
+export const AddFormFooter = styled('div')({});
+
+export const AddFormBottom = styled('div')({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginTop: 10,
+});
+
+export const AddFormRight = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
 });
